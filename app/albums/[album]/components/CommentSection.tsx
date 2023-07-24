c366b61261404256a3be2c50d3ddaa70fffa6d75
Isaac Yanez <isaac231467@gmail.com>
2023-07-24 10:48:48 -0500

    did alot of work on the card to add some really cool functionality

--- app/albums/[album]/components/CommentSection.tsx
+++ app/albums/[album]/components/CommentSection.tsx
@@ -8,11 +8,11 @@ export default function CommentSection() {
       <div className="text-lg mt-8  p-5 flex justify-evenly w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
         <h1 className="font-semibold text-2xl">
           Comments{" "}
-          <span className="underline font-light text-[#1DB954]">(10k)</span>
+          <span className="underline text-[#1DB954] font-thin">(10k)</span>
         </h1>
         <AlbumReviewModal />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-7">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-7 items-start">
         <ReviewCards />
         <ReviewCards />
         <ReviewCards />
@@ -20,6 +20,9 @@ export default function CommentSection() {
         <ReviewCards />
         <ReviewCards />
       </div>
+      <button className="mt-6 mb-6 bg-[#1DB954] rounded p-2 font-thin hover:cursor-pointer hover:bg-[#1ed760] transition-all duration-200 ease-in-out">
+        Read More
+      </button>
     </div>
   );
 }
--- app/albums/[album]/components/ReviewCards.tsx
+++ app/albums/[album]/components/ReviewCards.tsx
@@ -1,24 +1,53 @@
+"use client";
+import { useState } from "react";
+import { ArrowDown } from "react-feather";
+
 export default function ReviewCards() {
+  const [isTruncated, setIsTruncated] = useState(true);
+
+  const toggleTruncate = () => {
+    setIsTruncated(!isTruncated);
+  };
+
   return (
-    <div className="mt-5 w-full max-w-[350px] h-[125px] bg-[#353535] rounded-[20px] flex items-center justify-start backdrop-blur-[10px] transition-all duration-500 ease-in-out transform hover:scale-105 hover:cursor-pointer">
-      <div className="w-[70px] h-[70px] ml-[10px] rounded-[10px] bg-gradient-to-r from-[#d7cfcf] to-[#9198e5] transition-all duration-500 ease-in-out hover:from-[#9198e5] hover:to-[#712020]"></div>
-      <div className="w-[calc(100%-110px)] ml-[10px] text-white font-poppins">
-        <div className="flex items-center justify-between">
-          <div>
-            <p className="text-[18px] font-bold">This Album Sucks</p>
-            <div className="flex gap-2">
-              <p className="text-[12px] font-light">Isaac69</p>
-              <span>*****</span>
+    <div className="relative mt-5 w-full max-w-[350px] bg-[#353535] rounded-[20px] flex flex-col items-center justify-start backdrop-blur-[10px] transition-all duration-500 ease-in-out transform hover:scale-105 hover:cursor-pointer py-[15px]">
+      <div className="flex items-start w-full">
+        <div className="w-[70px] h-[70px] ml-[10px] rounded-[10px] bg-gradient-to-r from-[#d7cfcf] to-[#9198e5] transition-all duration-500 ease-in-out hover:from-[#9198e5] hover:to-[#712020]"></div>
+        <div className="mb-4 w-[calc(100%-110px)] ml-[10px] text-white font-poppins">
+          <div className="flex items-center justify-between">
+            <div>
+              <p className="text-[18px] font-bold">This Album Sucks</p>
+              <div className="flex gap-2">
+                <p className="text-[12px] font-light">Isaac69</p>
+                <span>*****</span>
+              </div>
             </div>
+            <span className="text-[12px]">12 min ago</span>
           </div>
-          <span className="text-[12px]">12 min ago</span>
+          <p
+            className={`text-[14px] font-light ${
+              isTruncated ? "truncate" : ""
+            }`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+            autem quaerat unde dolor cum deserunt itaque excepturi commodi,
+            molestias, sapiente delectus dicta! Dolorum voluptates earum optio
+            mollitia eius commodi soluta.
+          </p>
         </div>
-        <p className="text-[14px] font-light truncate">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, autem
-          quaerat unde dolor cum deserunt itaque excepturi commodi, molestias,
-          sapiente delectus dicta! Dolorum voluptates earum optio mollitia eius
-          commodi soluta.
-        </p>
+      </div>
+      <div
+        className="flex gap-3 items-center justify-center"
+        onClick={toggleTruncate}
+      >
+        {isTruncated ? "Read More" : "Read Less"}
+        <span>
+          {isTruncated ? (
+            <ArrowDown size={20} className="text-white" />
+          ) : (
+            <ArrowDown size={20} className="text-white transform rotate-180" />
+          )}
+        </span>
       </div>
     </div>
   );
